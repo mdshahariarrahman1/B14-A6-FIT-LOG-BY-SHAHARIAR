e55518503bc5b4fc1fc7d0bbd0573f467d26859e
mdshahariar98 <mdsidratulsaimshccho@gmail.com>
2026-09-25 23:51:19 +0600

    Context File Add

---
+++ src/context/FitLogContext.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { IfitLogType } from "@/type/type";
+import { ReactNode, useState, createContext } from "react";
+
+interface FitLogContextType {
+  plan: IfitLogType[];
+  setPlan: React.Dispatch<React.SetStateAction<IfitLogType[]>>;
+
+  saved: IfitLogType[];
+  setSaved: React.Dispatch<React.SetStateAction<IfitLogType[]>>;
+}
+
+export const FitLogContext =
+  createContext<FitLogContextType | null>(null);
+
+function FitLogProvider({ children }: { children: ReactNode }) {
+  const [plan, setPlan] = useState<IfitLogType[]>([]);
+  const [saved, setSaved] = useState<IfitLogType[]>([]);
+
+  const shareData = {
+    plan,
+    setPlan,
+    saved,
+    setSaved,
+  };
+
+  return (
+    <FitLogContext.Provider value={shareData}>
+      {children}
+    </FitLogContext.Provider>
+  );
+}
+
+export default FitLogProvider;
